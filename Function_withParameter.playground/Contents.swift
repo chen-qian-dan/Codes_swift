d9a3de6d0b1b35efd0829170603be885d3ced51f
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-01-25 15:11:20 +1030

    Function with parameter

---
+++ Function_withParameter.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+/*
+ func name (var1:type, var2:type) {
+ some code
+ }
+ 
+ name(var1:val, var2:val)
+ */
+
+//: define function
+func addTwoNumbers(a:Int, b:Int) {
+    print(a + b)
+}
+
+//: call function
+addTwoNumbers(a:80, b:9)
+
